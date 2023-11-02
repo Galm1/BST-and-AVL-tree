7a094f220dfbf29aa6c6095706564e40a91f7357
Elisha Garcia <garciaelisha93@yahoo.com>
2023-11-02 13:36:54 -0500

    Making struct for node/newnode

--- Part1.c
+++ Part1.c
@@ -1,13 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int arr[5] = {1,2,3,4,5};
+int arrayPD[5] = {1,2,3,4,5};
+
+struct Node 
+{
+	int data;
+	struct Node* left;
+	struct Node* right;
+	int height;
+};
+
+struct Node* newNode(int data)
+{
+	struct Node* node = (struct Node*) malloc(sizeof(struct Node));
+	node->data = data;
+	node->left = NULL;
+	node->right = NULL;
+	node->height = 0;
+	return (node);
+}
+
+
+
+
 
 int main(){
 	
 	
 	for (int i = 0; i < 5; i++){
-		printf("%d", arr[i]);
+		printf("%d", arrayPD[i]);
 	}
 	
 	
